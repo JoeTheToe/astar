54bee2d5401ae279b0088971e041642903c9ed1b
JoeTheToe <16163842@studentmail.ul.ie>
2019-03-11 19:39:47 +0000

    state #1

---
+++ State.java
@@ -0,0 +1,14 @@
+public class State {
+
+  byte stateArr[];
+  int height;
+
+  public State (byte arr[], int height) {
+    this.arr = stateArr[];
+    this.height = height;
+  }
+
+  public int getHeight(State s) {
+    return this.height;
+  }
+}
